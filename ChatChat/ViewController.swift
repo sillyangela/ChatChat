48ad81eabbafdd5a56352fdbf3c742e5b3264eec
sillyangela <qz625@nyu.edu>
2019-08-22 20:21:15 -0400

    Initial Commit

---
+++ ChatChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatChat
+//
+//  Created by Angela Zhang on 8/22/19.
+//  Copyright © 2019 qinyuanzhang.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
